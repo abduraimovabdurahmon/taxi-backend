f42d7f1757312e08d78c7ffea51219ccfa9ed34d
Abdurakhmon <abduraimovabdurahmon@gmail.com>
2023-10-24 14:23:35 +0500

    create login and register

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RegisterModule } from './authentication/register/register.module';
+import { LoginModule } from './authentication/login/login.module';
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb://localhost:27017")],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), RegisterModule, LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
